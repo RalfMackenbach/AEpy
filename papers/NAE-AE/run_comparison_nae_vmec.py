0a012a0ab702808a1fa2b2df45008cbf92cc9bf8
Ralf Mackenbach <ralf.mackenbach@gmail.com>
2023-04-24 16:01:44 +0200

    Comparison against vmec updated
    Is now leftward (i.e. low \varrho) extended using pyQSC. Good agreement across the board

--- papers/NAE-AE/run_comparison_nae_vmec.py
+++ papers/NAE-AE/run_comparison_nae_vmec.py
@@ -9,12 +9,8 @@ from    matplotlib                  import  rc
 
 
 
-def nae_ae_asymp(stel,omn,a_minor):
-    ae_fac  = 0.666834
-    r       = stel.r
-    eta     = np.abs(stel.etabar)
-    prefac  = np.sqrt(2) / (3 * np.pi)
-    return prefac * np.sqrt(r/ a_minor**2 / eta) * (omn)**3 * ae_fac
+
+
 # set up matplotlib
 rc('font',**{'family':'serif','serif':['Computer Modern Serif'], 'size': 12})
 rc('text', usetex=True)
@@ -24,7 +20,11 @@ rc('text', usetex=True)
 omnigenous = True
 
 # set lam res
-lam_res = 1001
+lam_res = 20001
+
+# set rho res
+rho_res = 25
+
 
 
 # omn/rho and omt/rho
@@ -33,19 +33,18 @@ omt = 0.0
 
 
 # Set up arrays
-res = 4
-rho_arr = np.logspace(-np.log10(201)/2,0.0,10) #np.logspace(np.log10(np.sqrt(1/201)),0,res) # rho = r / a_minor
-nae_ae  = np.empty_like(rho_arr)
-vmec_ae = np.empty_like(rho_arr)
-asym_ae_weak = np.empty_like(rho_arr)
-asym_ae_strong = np.empty_like(rho_arr)
+rho_arr = np.logspace(-3,0,rho_res) # rho = r / a_minor
+ae_num_qsc      = np.empty_like(rho_arr)
+ae_num_vmec     = np.empty_like(rho_arr)
+asym_ae_weak    = np.empty_like(rho_arr)
+asym_ae_strong  = np.empty_like(rho_arr)
 
 
 
 
 # make stellarator in VMEC
-file = "./configs/wout_precise_QA.nc"
-# file = 'wout_precise_QA_000_000000.nc'
+# file = "./configs/wout_precise_QA.nc"
+file = 'wout_precise_QA_000_000000.nc'
 mpi = MpiPartition()
 mpi.write()
 vmec = Vmec(file,mpi=mpi,verbose=True)
@@ -56,6 +55,11 @@ R_major = wout.Rmajor_p
 B_ref = 2 * np.abs(wout.phi[-1] / (2 * np.pi)) / (wout.Aminor_p)**2
 print(B_ref,R_major,a_minor,a_minor/R_major)
 
+
+# construct break point
+# set rho break point (< break: pyQSC, > break: VMEC)
+N_s=1/vmec.ds+1
+rho_break = np.sqrt(1/N_s)
 
 # make stellarator in QSC
 nphi = int(1e3+1)
@@ -74,19 +78,15 @@ for idx, rho in enumerate(rho_arr):
     omn_input = rho*omn
     omt_input = rho*omt
     stel.calculate()
-    NAE_AE = ae.AE_pyQSC(stel_obj = stel, r=stel.r, alpha=0.0, N_turns=1, nphi=nphi,
-                lam_res=lam_res,get_drifts=True,normalize='ft-vol',AE_lengthscale='None',a_minor=a_minor)
-    # NAE_AE.plot_geom()
-    NAE_AE.calc_AE(omn=stel.spsi*omn_input,omt=stel.spsi*omt_input,omnigenous=omnigenous)
-    # NAE_AE.plot_AE_per_lam()
-    nae_ae[idx] = NAE_AE.ae_tot
-
-    # loop over rho for vmec
-    VMEC_AE = ae.AE_vmec(vmec,rho**2,n_turns=1,lam_res=lam_res)
-    VMEC_AE.calc_AE(omn=-omn_input,omt=-omt_input,omnigenous=omnigenous)
-    # VMEC_AE.plot_precession()
-    # VMEC_AE.plot_AE_per_lam()
-    vmec_ae[idx] = VMEC_AE.ae_tot
+    if rho < rho_break:
+        NAE_AE = ae.AE_pyQSC(stel_obj = stel, r=stel.r, alpha=0.0, N_turns=1, nphi=nphi,
+                    lam_res=lam_res,get_drifts=True,normalize='ft-vol',AE_lengthscale='None',a_minor=a_minor)
+        NAE_AE.calc_AE(omn=stel.spsi*omn_input,omt=stel.spsi*omt_input,omnigenous=omnigenous)
+        ae_num_qsc[idx] = NAE_AE.ae_tot
+    if rho > rho_break:
+        VMEC_AE = ae.AE_vmec(vmec,rho**2,n_turns=1,lam_res=lam_res)
+        VMEC_AE.calc_AE(omn=-omn_input,omt=-omt_input,omnigenous=omnigenous)
+        ae_num_vmec[idx] = VMEC_AE.ae_tot
 
     # Asymptotic AE
     asym_ae_weak[idx] = NAE_AE.nae_ae_asymp_weak(omn_input,a_minor)
@@ -106,8 +106,9 @@ for idx, rho in enumerate(rho_arr):
 fig, ax = plt.subplots(1,1,figsize=(10,5),tight_layout=True)
 
 ax.loglog(rho_arr,asym_ae_weak,label=r'$\widehat{A}_\mathrm{asymp,weak}$',color='black',linestyle='dotted')
-ax.loglog(rho_arr,asym_ae_strong,label=r'$\widehat{A}_\mathrm{asymp,strong}$',color='black',linestyle='--')
-ax.loglog(rho_arr,vmec_ae,label=r'$\widehat{A}_\mathrm{vmec}$')
+# ax.loglog(rho_arr,asym_ae_strong,label=r'$\widehat{A}_\mathrm{asymp,strong}$',color='black',linestyle='--')
+ax.scatter(rho_arr,ae_num_qsc,label=r'$\widehat{A}_\mathrm{qsc}$',color='black',marker='x')
+ax.scatter(rho_arr,ae_num_vmec,label=r'$\widehat{A}_\mathrm{vmec}$',color='black',marker='o')
 # ax.loglog(rho_arr,nae_ae,label=r'$\widehat{A}_\mathrm{qsc}$')
 ax.set_ylabel(r'$\widehat{A}$')
 ax.set_xlabel(r'$\varrho$')
